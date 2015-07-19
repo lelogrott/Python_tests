126c034b19126462ba2b3bfd88a25917738306ac
lelogrott <grott.aurelio@gmail.com>
2015-07-19 15:32:08 -0300

    new tests added, everything ok

--- PDDE/testlibPDDE.py
+++ PDDE/testlibPDDE.py
@@ -4,8 +4,6 @@ from ctypes import *
 libPDDE = CDLL('/home/zaphod/Desktop/Python_tests/PDDE/libPDDE1.0')
 print(libPDDE)
 
-
-
 class noPDDE(Structure):
     pass
 
@@ -25,11 +23,27 @@ class PDDE(Structure):
 foo = POINTER(PDDE)()
 
 libPDDE.cria(byref(foo), sizeof(c_int))
+
 x=c_int(7)
 libPDDE.insere(foo, byref(x))
-print x
-x=c_int(9)
-print x
+
 libPDDE.busca(foo, byref(x))
 print x
 
+x=c_int(9)
+libPDDE.insere(foo, byref(x))
+
+libPDDE.busca(foo, byref(x))
+print x
+
+libPDDE.removeElemento(foo, byref(x))
+print x
+
+libPDDE.removeElemento(foo, byref(x))
+print x
+
+y=libPDDE.testaSeVazia(foo)
+if y==1:
+    print("VAZIA")
+else:
+    print("NAO VAZIA")  
